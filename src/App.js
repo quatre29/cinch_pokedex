a987d1b128b014183da423589679463114eaa497
Cam Lindsay <cameronlindsay@Cams-MacBook-Pro.local>
2022-05-31 13:56:07 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import "./App.css";
-import { useState } from "react";
-import Pokedex from "pokedex-promise-v2";
-
+import './App.css';
+import { useState } from 'react';
+const Pokedex = require('pokeapi-js-wrapper');
 function App() {
-  const [data, setData] = useState();
-
-  const options = {
-    protocol: "https",
-    hostName: "pokeapi.co",
-    versionPath: "/api/v2/",
-    cacheLimitL: 100 * 1000,
-    timeout: 5 * 1000,
+  const customOptions = {
+    protocol: 'https',
+    hostName: 'pokeapi.co',
+    versionPath: '/api/v2/',
+    cache: true,
+    timeout: 5 * 1000, // 5s
+    cacheImages: true,
   };
+  const P = new Pokedex.Pokedex(customOptions);
 
-  const P = new Pokedex(options);
-
-  P.getGenerationByName("generation-i").then((res) => {
-    console.log(res);
+  P.getPokedexByName('kanto').then(function (response) {
+    console.log(response);
   });
-
   return <div className="App"></div>;
 }
 
 export default App;
+
+// const [data, setData] = useState();
+
+// const options = {
+//   protocol: 'https',
+//   hostName: 'pokeapi.co',
+//   versionPath: '/api/v2/',
+//   cacheLimit: 100 * 10000,
+//   timeout: 5 * 1000,
+// };
+
+// const P = new Pokedex(options);
+
+// // P.getGenerationByName("generation-i").then((res) => {
+// //   console.log(res);
+// // });
+
+// P.getPokedexByName('kanto')
+//   .then((response) => {
+//     console.log(response);
+//   })
+//   .catch((error) => {
+//     console.log('There was an ERROR: ', error);
+//   });
