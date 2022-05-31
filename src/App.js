33fedcbbd9bc341955bb815e7f6ef865ada8c57b
CrypticSignal <theaudiophile@outlook.com>
2022-05-31 14:25:33 +0100

    Create array of pokemon names and images

--- src/App.js
+++ src/App.js
@@ -1,45 +1,42 @@
-import './App.css';
-import { useState } from 'react';
-const Pokedex = require('pokeapi-js-wrapper');
-function App() {
-  const customOptions = {
-    protocol: 'https',
-    hostName: 'pokeapi.co',
-    versionPath: '/api/v2/',
-    cache: true,
-    timeout: 5 * 1000, // 5s
-    cacheImages: true,
-  };
-  const P = new Pokedex.Pokedex(customOptions);
+import "./App.css";
+import { useState, useEffect } from "react";
+const Pokedex = require("pokeapi-js-wrapper");
 
-  P.getPokedexByName('kanto').then(function (response) {
-    console.log(response);
-  });
+const customOptions = {
+  protocol: "https",
+  hostName: "pokeapi.co",
+  versionPath: "/api/v2/",
+  cache: true,
+  timeout: 5 * 1000, // 5 seconds.
+  cacheImages: true,
+};
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const P = new Pokedex.Pokedex(customOptions);
+    const interval = {
+      offset: 0,
+      limit: 150,
+    };
+
+    const pokeData = [];
+
+    P.getPokemonsList(interval).then(function (response) {
+      response.results.map((pokemon, index) =>
+        pokeData.push({
+          name: pokemon.name,
+          image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+            index + 1
+          }.png`,
+        })
+      );
+    });
+    setData(pokeData);
+  }, []);
+
   return <div className="App"></div>;
 }
 
 export default App;
-
-// const [data, setData] = useState();
-
-// const options = {
-//   protocol: 'https',
-//   hostName: 'pokeapi.co',
-//   versionPath: '/api/v2/',
-//   cacheLimit: 100 * 10000,
-//   timeout: 5 * 1000,
-// };
-
-// const P = new Pokedex(options);
-
-// // P.getGenerationByName("generation-i").then((res) => {
-// //   console.log(res);
-// // });
-
-// P.getPokedexByName('kanto')
-//   .then((response) => {
-//     console.log(response);
-//   })
-//   .catch((error) => {
-//     console.log('There was an ERROR: ', error);
-//   });
